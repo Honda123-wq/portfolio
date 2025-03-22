414a3a8f129f60f195e2821643cbe4e421c657a9
Honda123-wq <syake6666@gmail.com>
2025-03-22 17:10:27 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -300,6 +300,50 @@ function App() {
                 </div>
               </div>
             </div>
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-100 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium ml-2">
+                    簡単メモアプリ
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    ReactとJavaScriptで簡単なメモアプリを作りました。デプロイ先はAWSを使用しています。主にStateの管理を注意して実装しています。初心者にはちょうど良いコードの内容になっていて、簡単に実装できています。
+                  </p>
+                  <a
+                    href="https://memo-app-taupe.vercel.app/"
+                    className="mt-3 text-green-500 items-center"
+                  >
+                    もっと見る→
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
