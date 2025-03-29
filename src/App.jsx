93c4c381b7463a1d4287d6a305a457afc0853381
Honda123-wq <syake6666@gmail.com>
2025-03-29 22:55:10 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -477,6 +477,45 @@ function App() {
                 </div>
               </div>
             </div>
+              <div className="md:w-1/3 p-4">
+              <div className="bg-gray-100 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium ml-2">
+                    Redux　簡単ショッピングアプリ構築（React + Redux Toolkit +
+                    TypeScript）
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    React × Redux
+                    の基礎をしっかり身につけるために制作したシンプルなRedux学習用アプリです。
+                    Redux Toolkitを使って、状態管理の導入方法を学習しました。
+                  </p>
+                  <a
+                    href="https://memo-app-ts.vercel.app/"
+                    className="mt-3 text-green-500 items-center"
+                  >
+                    もっと見る→
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
