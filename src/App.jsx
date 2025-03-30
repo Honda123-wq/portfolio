1f6d3a9ba761320d29f4b9bb3fc6e46765594ba4
Honda123-wq <syake6666@gmail.com>
2025-03-30 18:53:41 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -516,6 +516,46 @@ function App() {
                 </div>
               </div>
             </div>
+             <div className="md:w-1/3 p-4">
+              <div className="bg-gray-100 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M21.2635 2.29289C20.873 1.90237 20.2398 1.90237 19.8493 2.29289L18.9769 3.16525C17.8618 2.63254 16.4857 2.82801 15.5621 3.75165L4.95549 14.3582L10.6123 20.0151L21.2189 9.4085C22.1426 8.48486 22.338 7.1088 21.8053 5.99367L22.6777 5.12132C23.0682 4.7308 23.0682 4.09763 22.6777 3.70711L21.2635 2.29289ZM16.9955 10.8035L10.6123 17.1867L7.78392 14.3582L14.1671 7.9751L16.9955 10.8035ZM18.8138 8.98525L19.8047 7.99429C20.1953 7.60376 20.1953 6.9706 19.8047 6.58007L18.3905 5.16586C18 4.77534 17.3668 4.77534 16.9763 5.16586L15.9853 6.15683L18.8138 8.98525Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M2 22.9502L4.12171 15.1717L9.77817 20.8289L2 22.9502Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium ml-2">
+                    Reactで簡単ToDoアプリ
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    ReactとJavaScriptで簡単なToDoアプリを作りました。タスクの追加・タスクの完了チェック（チェックボックス）・完了したタスクの削除・残りタスク数の表示の機能を実装しています
+                  </p>
+                  <a
+                    href="https://react-todo-gilt-phi.vercel.app/"
+                    className="mt-3 text-green-500 items-center"
+                  >
+                    もっと見る→
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
