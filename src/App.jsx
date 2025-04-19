647731ee52a97c0f285270035015ce18c9d05004
Honda123-wq <syake6666@gmail.com>
2025-04-19 21:42:06 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ function App() {
                   <p>
                     ReactとJavaScriptで投稿機能つきのChatアプリ。仕様はReact,JavaScript,html,css,firebase,AWS。ログイン・ログアウト認証機能、メッセージ投稿機能、検索機能、お気に入り投稿機能を実装しています。
                   </p>
-                  <a href="#" className="mt-3 text-green-500 items-center">
+                  <a href="https://smilechat-home.web.app" className="mt-3 text-green-500 items-center">
                     もっと見る→
                   </a>
                 </div>
